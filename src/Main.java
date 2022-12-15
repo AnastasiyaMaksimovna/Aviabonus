80a1c2561cf112749f477e5747b7eecafcd6e74c
Neto test <sturochka98@gmail.com>
2022-12-15 20:47:42 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15673;
+        int rubPerMile = 20;
+        int bonus = price / rubPerMile;
+        System.out.println(bonus);
+    }
+}
